2a795a697216962537520ccf67fdf5eaef425015
phamvanoanh <oanhmrphamvan@gmail.com>
2017-12-09 12:26:22 +0700

    commit develop-1

--- Education/src/main/java/edu/fa/repository/StudentRepositoryImpl.java
+++ Education/src/main/java/edu/fa/repository/StudentRepositoryImpl.java
@@ -2,8 +2,8 @@ package edu.fa.repository;
 
 public class StudentRepositoryImpl {
 	
-	public String getStudentList() {
-		return "oanhpv";
-	}
+//	public String getStudentList() {
+//		return "oanhpv";
+//	}
 
 }
