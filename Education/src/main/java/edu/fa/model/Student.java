7ca73d71403b25f8f2c765decbbe9656a0f53c94
phamvanoanh <oanhmrphamvan@gmail.com>
2017-12-09 12:11:12 +0700

    Frirst lesson spring

---
+++ Education/src/main/java/edu/fa/model/Student.java
@@ -0,0 +1,34 @@
+package edu.fa.model;
+
+public class Student {
+	
+	private String name;
+	
+	private String location;
+	
+	public Student() {
+	}
+
+	public Student(String name, String location) {
+		super();
+		this.name = name;
+		this.location = location;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
