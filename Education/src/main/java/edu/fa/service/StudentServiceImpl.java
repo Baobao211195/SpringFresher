8e599cfcca598cc2bc1f188af3320078eae30c16
phamvanoanh <oanhmrphamvan@gmail.com>
2017-12-10 20:53:34 +0700

    Commit setter injection

--- Education/src/main/java/edu/fa/service/StudentServiceImpl.java
+++ Education/src/main/java/edu/fa/service/StudentServiceImpl.java
@@ -4,18 +4,28 @@ import java.util.List;
 
 import edu.fa.model.Student;
 import edu.fa.repository.StudentRepository;
-import edu.fa.repository.StudentRepositoryImpl;
 
 public class StudentServiceImpl implements StudentService {
 	
-	private StudentRepository studentRepositoryImpl = new StudentRepositoryImpl();
+	// setter injection
+	private StudentRepository studentRepository ;
 	
-	
-	
+
+	public StudentRepository getStudentRepository() {
+		return studentRepository;
+	}
+
+	// setter injection
+	// map with property in bean StudentService
+	public void setStudentRepositorys(StudentRepository studentRepository) {
+		System.out.println("Mapping with property in context.xml --> setter injection");
+		this.studentRepository = studentRepository;
+	}
+
 	/* (non-Javadoc)
 	 * @see edu.fa.service.StudentService#getAllStudent()
 	 */
 	public List<Student> getAllStudent() {
-		return studentRepositoryImpl.getAllStudent();
+		return studentRepository.getAllStudent();
 	}
 }
